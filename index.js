46396b6459c28737bfdc147dacdab317ea875c34
Qu1etboy <nonzagreanthai@gmail.com>
2022-11-13 06:24:10 +0700

    Set express to look in folder views

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 app.use(express.json());
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
